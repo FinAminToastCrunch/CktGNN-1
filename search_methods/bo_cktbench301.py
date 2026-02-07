b6aa84d34ce62f7b0ee5cc707388142b5c22d8cb
Fin Amin <shafin.amin@gmail.com>
2026-02-07 00:40:31 -0500

    Import pickle module in bo_cktbench301.py
    Added import for pickle module.

--- search_methods/bo_cktbench301.py
+++ search_methods/bo_cktbench301.py
@@ -10,6 +10,7 @@ import numpy as np
 from collections import defaultdict
 import matplotlib.pyplot as plt
 from search_methods.utils_search import *
+import pickle
 
 def bo_expected_improvement_search(embedding_path, total_rounds, save_path, init_size, k, seed):
     """ implementation of CATE-DNGO-LS on the NAS-Bench-101 search space """
